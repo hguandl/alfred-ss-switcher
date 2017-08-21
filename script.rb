ad2f1c17249cad7e4bc5795a80726e7cbb763426
hguandl <hguandl@gmail.com>
2017-08-21 20:28:24 +0800

    Add timeout for ping

--- script.rb
+++ script.rb
@@ -1,14 +1,20 @@
 # Grab the query that the user typed (this is provided by Alfred).
 theQuery = "{query}"
 
+#theQuery = "google"
+# for debug
+
 # Assemble an array of each matching server.
 # The -i flag ignores case.
 servers = `ls -1 /usr/local/etc/shadowsocks.d | grep -i [^/]*#{Regexp.quote(theQuery)}[^/]*$`.split(".json\n")
 # Start the XML string that will be sent to Alfred. This just uses strings to avoid dependencies.
 xmlString = "<?xml version=\"1.0\"?>\n<items>\n"
 servers.each do | server |
-	ping = `ping -c1 #{Regexp.quote(server)} | grep time | awk '{print $7 $8}'`
+	ping = `ping -c1 -t1 #{Regexp.quote(server)} | grep time | awk '{print $7 $8}'`
 	iconValue = "/usr/local/etc/ss-icons.d/#{server}.png"
+	if ping == ""
+		ping = "time>1s"
+	end
 	# The icon type sent to Alfred is 'fileicon' (taken from a file). This assumes that a .app was found.
 	# Assemble this item's XML string for Alfred. See http://www.alfredforum.com/topic/5-generating-feedback-in-workflows/
 	thisXmlString = "\t<item uid=\"#{server}\" arg=\"#{server}\">
